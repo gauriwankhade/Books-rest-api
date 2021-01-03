58dd716157bf1a96e36f71d3df003d0b098cf8f5
gauriwankhade <gauriwankhade888@gmail.com>
2021-01-03 15:41:07 +0530

    added search filter

--- test-project/books/views.py
+++ test-project/books/views.py
@@ -3,6 +3,7 @@ from .serializers import *
 from rest_framework import viewsets,permissions
 from django.http import JsonResponse
 from rest_framework.decorators import action
+from rest_framework import filters
 
 # Create your views here.
 
@@ -11,16 +12,11 @@ class BookViewSet(viewsets.ModelViewSet):
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'url']
+
     def get_queryset(self):
         queryset = Book.objects.all()
         return queryset
 
-    @action(detail=False)     
-    def search(self,request):
-        search_input = self.request.data
-        result = Book.objects.filter(name__icontains=search_input['name'])
-        data_json = list(result.values())
-        return JsonResponse({'Book Matches': data_json})
-       
-
-                        
+    
